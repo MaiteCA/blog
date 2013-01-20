df41e728282f8f022b9263cb37fd30e021b05123
Nicolas <bluescreenjunky@gmail.com>
2013-01-20 22:38:12 +0100

    Ajout des boutons sociaux (Google+, Twitter)

--- views/showpost.tpl.php
+++ views/showpost.tpl.php
@@ -6,6 +6,12 @@
     </time>
     <h1><?= $post['title'] ?></h1>
     <?= $post['content']; ?>
+    <div class=social>
+        <a href="https://twitter.com/share" class=twitter-share-button data-lang=fr data-count=none>Twitter</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        <div class=g-plusone data-size=medium data-width=120 data-annotation=none></div>
+        <script type="text/javascript">window.___gcfg = {lang: 'fr'};(function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://apis.google.com/js/plusone.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>
+    </div>
     <section class='comments'>
         <h1>commentaires</h1>
         <form action="addcomment" method="POST" id=comment-form>
