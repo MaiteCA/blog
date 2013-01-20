0c2671dbc9e122a5e11303007f38bf291178b2f8
Nicolas <bluescreenjunky@gmail.com>
2013-01-20 21:45:55 +0100

    Ajout de commentaires sur l'index.

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 // CONFIGURATION
-setlocale(LC_ALL,'fr_FR','fra');
-define('DSN','sqlite:database/db');
+setlocale(LC_ALL,'fr_FR','fra');        //Locale (langue pour la date etc.)
+define('DSN','sqlite:database/db');     //Adresse de connexion à la BDD.
 
 // INCLUDES
-require "database/model.php";
-require "controllers/functions.php";
+require "database/model.php";           //Le modèle (base de donnée)
+require "controllers/functions.php";    //Les fonctions communes
 
 // AFFICHAGE DE LA PAGE (ROUTE)
+
+//Liste des pages autorisées
 $authorized_pages = [
     'showpost',
     'showposts',
@@ -16,10 +18,11 @@ $authorized_pages = [
     'admin',
 ];
 
+//Si on a le paramètre page ET qu'elle fait partie des pages autorisées...
 if (isset($_GET['page']) && in_array($_GET['page'], $authorized_pages)) {
-   $page = $_GET['page'];
-} else {
-   $page = 'showposts';
+   $page = $_GET['page'];   // ... On utilise la page en question
+} else {                    // Sinon ...
+   $page = 'showposts';     // ... On utilise la page par défaut.
 }
 
 // Appel au controller qui va générer les données à afficher :
